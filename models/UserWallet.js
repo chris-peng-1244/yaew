621c974a6c0a8af551b4d3713d11e7827a15cd5c
Peng Chi <pengchi@zjsk.onaliyun.com>
2018-03-29 18:05:17 +0800

    Add check balance method

--- models/UserWallet.js
+++ models/UserWallet.js
@@ -1,11 +1,30 @@
 const redis = require('../utils/Redis');
+const web3 = require('../utils/Web3');
 const USER_WALLET_HASH = process.env.APP_NAME + '_user_wallets';
 
-exports.create = async (address, privateKey) => {
-  const exists = await redis.hexistsAsync(USER_WALLET_HASH, address);
-  if (exists) {
-    return false;
+exports.create = async () => {
+  const account = web3.eth.accounts.create(web3.utils.randomHex(32));
+  const exists = await redis.hexistsAsync(USER_WALLET_HASH, account.address);
+  if (!exists) {
+    await redis.hsetAsync(USER_WALLET_HASH, account.address, account.privateKey);
   }
-  await redis.hsetAsync(USER_WALLET_HASH, address, privateKey);
-  return true;
+  return account;
+};
+
+exports.get = async (address) => {
+  const privateKey = await redis.hgetAsync(USER_WALLET_HASH, address);
+  if (privateKey.length != 66) {
+    return null;
+  }
+  return web3.eth.accounts.privateKeyToAccount(privateKey);
+};
+
+/**
+ * 
+ * @param string address 
+ * @throws When the address is invalid or the network is lost.
+ */
+exports.getBalance = async (address) => {
+    const balanceInWei = await web3.eth.getBalance(address);
+    return web3.utils.fromWei(balanceInWei, "ether");
 };
--- routes/wallet.js
+++ routes/wallet.js
@@ -4,30 +4,48 @@ const boom = require('boom');
 const web3 = require('../utils/Web3');
 const UserWallet = require('../models/UserWallet');
 
-router.get('/:address', (req, res, next) => {
-  return res.json({
-    code: 0,
-    data: req.params.address,
-  })
+router.get('/:address', async (req, res, next) => {
+  try {
+    const balance = await UserWallet.getBalance(req.params.address);
+    return res.json({
+      code: 0,
+      data: balance,
+    })
+  } catch (e) {
+    return next(boom.badRequest(`Can't find balance of address: ${req.params.address}`));
+  }
 });
 
 router.post('/', async (req, res, next) => {
-  const account = web3.eth.accounts.create(web3.utils.randomHex(32));
   try {
-    const success = await UserWallet.create(account.address, account.privateKey);
-    if (success) {
-      return res.json({
-        code: 0,
-        data: {
-          address: account.address,
-          privateKey: account.privateKey,
-        }
-      });
-      return res.json(boom.badImplementation("POST /wallet save account failed"));
-    }
+    const account = await UserWallet.create();
+    return res.json({
+      code: 0,
+      data: {
+        address: account.address,
+        privateKey: account.privateKey,
+      },
+    });
   } catch (e) {
     next(boom.badImplementation("POST /wallet "+e.message));
   }
+});
+
+router.post('/transfer', async (req, res, next) => {
+  const from = req.body.from;
+  const to = req.body.to;
+  const token = Token.create(req.body.tokenType, req.body.amount);
+  const { error, hash } = await UserWallet.transfer(from, to, token);
+  if (error === UserWallet.INSUFFICIENT_TOKEN) {
+    return next(boom.badRequest("User wallet doesn't have enough token to transfer"));
+  } else if (error !== null) {
+    return next(boom.badImplementation("Transfer failed: "+error));
+  }
+
+  return res.json({
+    code: 0,
+    data: hash,
+  });
 });
 
 module.exports = router;
